7ad2b236de44f14515d3e2b178bb609626477f7c
Garrett Heald <gheald@calpoly.edu>
2017-11-08 23:23:25 -0800

    added page 'request_access' page for creating an account

--- DDAPIwebsite/DDAPIwebsite/views.py
+++ DDAPIwebsite/DDAPIwebsite/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from . import templates
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def request_access(request):
+    return render(request, "request_access.html")
 
