2a7d56714e294151536ca21129b8b2c30fcfea64
Vivek Bhakta <vbhakta8@gmail.com>
2018-03-05 16:19:48 -0800

    Added JSON ification to sql query return statement.

--- DDAPIwebsite/DDAPIwebsite/queryDB.py
+++ DDAPIwebsite/DDAPIwebsite/queryDB.py
@@ -1,11 +1,16 @@
 from django.db import connections
-
+import json
+import datetime
 
 def get_from_DB(query):
-    cursor = connections['apiDB'].cursor()
-    cursor.execute(query)
-    rows = cursor.fetchall()
-    return rows
+    # cursor = connections['apiDB'].cursor()
+    # cursor.execute(query)
+    # rows = cursor.fetchall()
+    # This will get the
+    cur2 = connections['apiDB'].cursor(MySQLdb.cursors.DictCursor)
+    cur2.execute(query)
+    rows2 = cur2.fetchall()
+    return rows2
 
 
 def get_json_from_backend(query):
@@ -15,6 +20,16 @@ def get_json_from_backend(query):
     # if not in cache, then query database
 
     rows = get_from_DB(query)
+
+    def clean_output(o):
+        # Checked for single quote strings
+        if isinstance(o, str):
+            return o.__str__()
+        if isinstance(o, datetime.datetime):
+            # set time to Epoch PST
+            return o.strftime('%s')
+
+    rows = json.dumps(rows, default=clean_output)
 
     # convert to json here
 
